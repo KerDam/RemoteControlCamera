db69f0639962673ad722dcedd14a0454175b98d0
AlexCxx <coal@hotmail.fr>
2019-03-26 22:44:38 +0100

    link between front & back end effective

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, Text, View
 from flask_bootstrap import Bootstrap
+from camera import VideoCamera
+import cv2
 
 app = Flask(__name__)
 nav = Nav(app)
@@ -10,7 +12,7 @@ Bootstrap(app)
 @nav.navigation('site_navbar')
 def create_navbar():
     home_view = View('Home', 'home')
-    live_view = View('Live','live')
+    live_view = View('Live','live',status=0) #init with OFF state of the webcam
     gallery_view = View('Gallery','gallery')
     return Navbar('RemoteCamera', home_view, live_view, gallery_view)
 
@@ -18,25 +20,50 @@ def create_navbar():
 def home():
     return render_template('base.html')
 
-@app.route('/live')
-def live():
-    return render_template('hello.html')
+    
+@app.route('/live/<int:status>')
+def live(status):
+    #return render_template('live.html')
+    if status:        
+        return render_template('live.html', status=status)
+    else:        
+        return render_template('live.html', status=status)
+    
 
 @app.route('/gallery')
 def gallery():
-    return render_template('hello.html')
+    return render_template('live.html')
+
+#base webcam code
 
 @app.route('/camera/<int:status>')
 def camera(status):
     if status:
-        cameraOnOff(status)
+        #cameraOnOff(status)
         return render_template('camera.html', status=status)
     else:
-        cameraOnOff(status)
+        #cameraOnOff(status)
         return render_template('camera.html', status=status)
+    
+# Webcam handling
 
-def cameraOnOff(status):
-    if status:
-        print("Camera ON")
-    else:
-        print("Camera OFF")
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    frame = camera.get_frame()
+    yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+#def cameraOnOff(status):
+    #if status:
+        #print("Camera ON")
+    #else:
+        #print("Camera OFF")
