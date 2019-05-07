ed7f5d2831e4ef0bebbdb5f16b3d394f14852fca
AlexCxx <coal@hotmail.fr>
2019-05-07 16:56:59 +0200

    correction of camera function tests

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2
+import datetime, time, imutils
 
 class VideoCamera(object):
     def __init__(self):
@@ -25,3 +26,81 @@ class VideoCamera(object):
     def get_frame(self):
         success, frame = self.video.read()
         return frame
+
+# Webcam handling
+
+FRAME_SAVING_INTERVAL = 2 #seconds
+MONTION_DETECTION_SENSITIVITY = 500
+
+def save_frame(frame):
+    timestr = time.strftime("%Y%m%d_%H%M%S") # Create a timestamp
+    cv2.imwrite('captures/'+timestr+'.jpg',frame) #Save the curren frame
+
+def motion_detect(firstFrame, gray, cv2):
+        # compute the absolute difference between the current frame and
+    # first frame
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < MONTION_DETECTION_SENSITIVITY:    
+            continue
+        return 1
+    return 0
+def gen(camera):
+    
+    firstFrame = None
+    previous_time = time.time() # Get the time before motion detection
+    text = "Unoccupied"
+    
+    while True:
+        #MOTION DETECTION
+        
+        frame = camera.get_frame()
+
+        # if the frame could not be grabbed, then we have reached the end
+        # of the video
+        if frame is None:
+            break
+
+        # resize the frame, convert it to grayscale, and blur it
+        frame = imutils.resize(frame, width=500)
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+        
+        # if the first frame is None, initialize it
+        if firstFrame is None:
+            firstFrame = gray_frame
+            continue
+        
+        if (motion_detect(gray_frame, firstFrame, cv2) == 1):
+            text = "Occupied"
+            
+            #Delay for saving frames
+            now_time = time.time()
+            
+            if abs(int(now_time - previous_time)) > FRAME_SAVING_INTERVAL :
+                print("capture")
+                previous_time = time.time()    
+                save_frame(frame)
+        else:
+            text = "Unoccupied"
+            
+        #add Room status text to video feed
+        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+            
+        #convert the frame to jpg image and save it
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
--- test_camera.py
+++ test_camera.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
-from app import *
+import fnmatch
+import os
+from camera import *
 
 def test_motion_detect_no_move():
     cap = cv2.VideoCapture('open_cv_tests/1.avi')
@@ -32,7 +34,7 @@ def test_motion_detect_no_move():
         else: 
             break
 
-    assert detection == None
+    assert detection == 0
 
     # When everything done, release the video capture object
     cap.release()
@@ -69,5 +71,34 @@ def test_motion_detect_move():
 
     assert detection == 1
 
+    # When everything done, release the video capture object
+    cap.release()
+
+def test_save_frame():
+    cap = cv2.VideoCapture('open_cv_tests/2.avi')
+    detection = 5
+    firstFrame = None
+    timestr = 0
+    # Read until video is completed
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            timestr = time.strftime("%Y%m%d_%H%M%S") # Create a timestamp
+
+            save_frame(frame)
+            break    
+        # Break the loop
+        else: 
+            break
+    frame_found = 0
+    
+    for root, dirs, files in os.walk("captures/"):  
+        for entry in files:
+            print(entry)
+            print(timestr)
+            if fnmatch.fnmatch(entry, timestr + ".jpg"):
+                frame_found = 1
+    assert frame_found
     # When everything done, release the video capture object
     cap.release()
